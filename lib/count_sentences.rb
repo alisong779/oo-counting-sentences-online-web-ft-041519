270907ae5375edfe1bb3a25fee385c9aebe05e58
Alison Golden <alisong779@yahoo.com>
2019-05-09 19:00:26 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -19,3 +19,7 @@ class String
     new_array.length 
   end
 end
+
+#1.splitting the string by punctuataion .?! (using regex) and adding to a new array using reject 
+#2.then iterating through the array and removing any empty strings and assigning the result of the iteration to new array 
+#3.returning the length of the new array 
